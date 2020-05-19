04c4983c7e4c2465559b2f1f8e546c78b18f2520
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2020-05-19 23:49:55 +0200

    updated model, not working

--- longestpathknapsack/longestpathknapsack.py
+++ longestpathknapsack/longestpathknapsack.py
@@ -25,11 +25,25 @@ def solve(a, p, b):
         for i in range(b-a[j]):
             arcs.append((i, j-1, i+a[j],j, a[j]))
 
+   #  # skip arcs
+    #  for j in range(1, nitems):
+    #      for i in range(b):
+    #          arcs.append(((i,j-1),(i,j),0))
+    #
+    #  # waste arcs
+    #  for j in items:
+    #      for i in range(b-1):
+    #          arcs.append(((i,j),(i+1,j),1))
+    #
+    #  # items arcs
+    #  for j in range(1, nitems):
+    #      for i in range(b-a[j]):
+    #          arcs.append(((i, j-1), (i+a[j],j), a[j]))
+#
     # ------------------------------------------------------------------------
     arcs = tuplelist(arcs)
 
     print(arcs)
-    print(arcs_1)
     # Model
     model = Model("Flowbased knapsack")
     model.modelSense = GRB.MAXIMIZE
@@ -38,16 +52,38 @@ def solve(a, p, b):
     # not (value 0)
     x = {}
     for arc in arcs:
-        x[(arc[0], arc[1])] = model.addVar(name="x_(%s,%s),(%s,%s)" %
-                                           (arc[0][0], arc[0][1], arc[1][0],
-                                            arc[1][1]),
+        x[(arc[0:2], arc[2:4])] = model.addVar(name="x_(%s,%s),(%s,%s)" %
+                                           (arc[0], arc[1], arc[2],
+                                            arc[3]),
                                            vtype=GRB.CONTINUOUS,
-                                           obj=arc[2])
+                                           obj=arc[4])
     # Update the model to make variables known.
     # From now on, no variables should be added.
+
     model.update()
 
     # TODO: Add your constraints ----------------------------------------------
+    s = (0,0)
+    t = (b,nitems)
+
+    for arc in arcs:
+            print(arcs.select('*', arc[0,2]))
+            # right hand side
+            rhs = 0
+            if arc[0:2] == s:
+                rhs = 1
+            if arc[0:2] == t:
+                rhs = -1
+            # now the flow balance
+            model.addConstr(
+                # outgoing edges
+                quicksum(x[(arc[0:2],j)] for j in list(t[1] for t in
+                    arcs.select(arc[0:2],'*'))) -
+                # incoming edges
+                quicksum(x[(j,arc[0:2])] for j in list(t[0] for t in
+                    arcs.select('*',arc[0,2])))
+                == rhs, name="flow_"+str(i)
+                )
 
     # -------------------------------------------------------------------------
 
