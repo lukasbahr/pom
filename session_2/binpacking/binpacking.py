aeb53ca7b2f94bacb6a33c9b6686d8eac1ef1f6d
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2020-05-26 12:10:30 +0200

    added solution for bp with linking

--- session_2/binpacking/binpacking.py
+++ session_2/binpacking/binpacking.py
@@ -1,19 +1,19 @@
 from gurobipy import *
 
-def solve(m, a, b):
+def solve(items, bins, conflicts, capacity, weight):
     # Model
     model = Model("Binpacking with conflicts")
 
   # Decision variable x_i_j indicates whether Item i is packed into Bin j (value 1) or not (value 0).
     x = {}
-    for i in range(len(a)):
-        for j in range(m):
+    for i in items:
+        for j in bins:
             # TODO: Adjust additional attributes (lb, ub, vtype, obj). Do NOT change the name!
             x[i,j] = model.addVar(vtype='b', name="x_%s_%s" % (i,j))
 
     # Decision variable y_j indicates whether Bin j is used (value = 1) or not (value = 0).
     y = {}
-    for j in range(m):
+    for j in bins:
         # TODO: Adjust additional attributes (lb, ub, vtype, obj). Do NOT change the name!
         y[j] = model.addVar(vtype='b', name="y_%s" % (j))
 
@@ -25,19 +25,20 @@ def solve(m, a, b):
     # TODO: Add the linear constraints of the model. Nonlinearities in the model, e.g.,
     # multiplication of two decision variables, results in a score of 0!
 
-    for j in range(m):
-        model.addConstr(quicksum(x[i,j]*a[i] for i in range(len(a)))
-                <= b)
+    for j in bins:
+        model.addConstr(quicksum(x[i,j]*weight[i] for i in range(len(items)))
+                <= capacity * y[j])
 
-    for i in range(len(a)):
-        model.addConstr(quicksum(x[i,j] for j in range(m)) == 1)
+    for j in bins:
+        for i,k in conflicts:
+            model.addConstr((x[i,j]+x[k,j])<=1)
 
-    for i in range(len(a)):
-        for j in range(m):
-            model.addConstr(x[i,j] <= y[j])
+    for i in items:
+        model.addConstr(quicksum(x[i,j] for j in range(len(bins))) == 1)
 
 
-    model.setObjective(quicksum(y[j] for j in range(m)),GRB.MINIMIZE)
+
+    model.setObjective(quicksum(y[j] for j in bins),GRB.MINIMIZE)
     model.ModelSense = GRB.MINIMIZE
 
 
@@ -48,3 +49,4 @@ def solve(m, a, b):
       # Return the model: Do not change/remove this line - it is crucial for our scoring method
     # and removal may lead to a score of 0!
     return model
+
