75b240bc241cbb348082a7b7c336f1725eec20f9
matthias <matthias.freitag@rwth-aachen.de>
2020-06-15 13:03:24 +0200

    list of unique pairs

--- university_timetabling/timetables_m.py
+++ university_timetabling/timetables_m.py
@@ -22,8 +22,18 @@ def solve(full_path_instance):
     y_time = {}
     y_days = {}
     T = []      # list of all timeslots for easier quicksums
-    L = []      # list of all lecture-pairs by same teachers
-    C = []      # list of all lecture-pairs in same curricula
+    L = []      # list of all unique lecture-pairs by same teachers
+    C = []      # list of all unique lecture-pairs in same curriculum
+
+    for u in q:
+        u_pair = list(itertools.combinations(q[u], 2))
+        C += u_pair
+    C = list(set(C))
+
+    for a in c:
+        a_pair = list(itertools.combinations(c[a], 2))
+        L += a_pair
+    L = list(set(L))
 
     for i in range(n):
         for k in l:
@@ -32,16 +42,16 @@ def solve(full_path_instance):
         for j in range(m):
             T.append((i,j))
 
-            for k in l:     # variable to assign a course k to a timeslot (i,j)
+            for k in l:         # variable to assign a course k to a timeslot (i,j)
                 x[k,(i,j)] = model.addVar(vtype='b', name="x_%s_(%s,%s)" % (k,i,j))
 
-            for a in c:     # variable to activate cost for assistants to give lecture
-                y_assi[a,(i,j)] = model.addVar(vtype=GRB.INTEGER,
-                        name="y_assi_%s_(%s,%s)" %(a,i,j))
+            for L_pair in L:         # variable to activate cost for assistants to give lecture
+                y_assi[L_pair,(i,j)] = model.addVar(vtype=GRB.INTEGER,
+                        name="y_assi_(%s_%s)_(%s,%s)" %(L_pair[0], L_pair[1], i, j))
 
-            for u in q:     # variable to avtivate penalty for courses of same curriculum
-                y_curr[u,(i,j)] = model.addVar(vtype=GRB.INTEGER,
-                        name="y_curr_%s_(%s,%s)" %(u,i,j))
+            for C_pair in C:    # variable to activate penalty for courses of same curriculum
+                y_curr[C_pair, (i, j)] = model.addVar(vtype=GRB.INTEGER,
+                                           name="y_curr_(%s_%s)_(%s,%s)" % (C_pair[0], C_pair[1], i, j))
 
     for v in t:
         for w in t[v]:      # variable to activate penalty for using forbidden timeslots
@@ -53,6 +63,7 @@ def solve(full_path_instance):
         y_days[k] = model.addVar(vtype = GRB.INTEGER, name = "y_days_%s" %(k))
 
     model.update()
+    model.write('model.lp')
 
     for k in l:
         model.addConstr(quicksum(x[k,(i,j)] for (i,j) in T) == l[k])
@@ -64,25 +75,25 @@ def solve(full_path_instance):
                     == 0 )
 
     for (i,j) in T:
-        for a in c:
-            model.addConstr(quicksum(x[k,(i,j)] for k in c[a]) <=
-                    1+y_assi[a,(i,j)])
+        for L_pair in L:
+            model.addConstr(quicksum(x[k,(i,j)] for k in L_pair) <=
+                    1+y_assi[L_pair,(i,j)])
 
-        for u in q:
-            model.addConstr(quicksum(x[k,(i,j)] for k in q[u]) <=
-                    1+y_curr[u,(i,j)])
+        for C_pair in C:
+            model.addConstr(quicksum(x[k,(i,j)] for k in C_pair) <=
+                    1+y_curr[C_pair,(i,j)])
 
     for v in t:
         for w in t[v]:
             model.addConstr(x[v,w]==0+y_time[v,w])
 
-    model.setObjective(1 * quicksum(y_assi[a,(i,j)] for a in c for (i,j) in T)
-                       +0.1 * quicksum(y_curr[u,(i,j)] for u in q for (i,j) in T)
+    model.setObjective(1 * quicksum(y_assi[L_pair,(i,j)] for L_pair in L for (i,j) in T)
+                       +0.1 * quicksum(y_curr[C_pair,(i,j)] for C_pair in C for (i,j) in T)
                        +10 * quicksum(y_time[v,w] for v in t for w in t[v])
                        +0.1 * quicksum(y_days[k] for k in l ), GRB.MINIMIZE)
 
     model.update()
-    model.write('model.lp')
+    #model.write('model.lp')
 
     # Solve model
     model.optimize()
