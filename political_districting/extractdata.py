f7eaa11fba367bd5914bc4d35b9e9ca80d0fd822
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2020-07-01 22:57:35 +0200

    updated dataframe

--- political_districting/extractdata.py
+++ political_districting/extractdata.py
@@ -9,25 +9,29 @@ def getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung):
     df_Border = getDF_SHPFile(shp_file, drop)
     df_Center = getDF_SHPFileCenteroid(shp_file_centeroid, drop)
 
-    df = df_Border.merge(df_Center, left_on = 'plz', right_on = 'plz')
-    df['plz']=df['plz'].astype(int)
-    df = df.merge(df_PLZ_BL[['bundesland', 'plz']], left_on = 'plz', right_on = 'plz')
+    if len(df_Border)  == len(df_Center) == len(df_PLZ_BL):
+        df_Border = df_Border.merge(df_PLZ_BL[['bundesland', 'plz']], left_on = 'plz', right_on = 'plz')
+        df_Center = df_Center.merge(df_PLZ_BL[['bundesland', 'plz']], left_on = 'plz', right_on = 'plz')
 
-    return df
+        return df_Border, df_Center
+
+    else:
+        print("Rows of dataframes are not matching.")
+
 
 def getDF_SHPFileCenteroid(path, drop):
     df = gpd.read_file(path)
     df.drop(df[~df['plz'].str.contains('|'.join(str(x) for x in drop))].index, axis=0, inplace=True)
-    df.rename(columns={'geometry':'geometry_center'}, inplace = True)
-    df.drop(['note', 'einwohner', 'qkm'], axis=1, inplace = True)
+    df.drop(['note'], axis=1, inplace = True)
+    df['plz'] = df['plz'].astype(int)
 
     return df
 
 def getDF_SHPFile(path, drop):
     df = gpd.read_file(path)
     df.drop(df[~df['plz'].str.contains('|'.join(str(x) for x in drop))].index, axis=0, inplace=True)
-    df.rename(columns={'geometry':'geometry_border'}, inplace = True)
     df.drop(['note'], axis=1, inplace = True)
+    df['plz'] = df['plz'].astype(int)
 
     return df
 
--- political_districting/politicaldistricting.py
+++ political_districting/politicaldistricting.py
@@ -12,8 +12,29 @@ def solve(G, k, req_p):
 
 
 def createGraph(df):
-    print(df)
+    G = nx.Graph()
+    for index, row in df.iterrows():
+        G.add_node(row['plz'], population=row['einwohner'])
 
+    #  print(list(G.nodes(data='population')))
+
+
+    return G
+
+def findSharedBorders(df):
+    df["NEIGHBORS"] = None  # add NEIGHBORS column
+
+    for index, row in df.iterrows():
+        # get 'not disjoint' countries
+
+        print(row['geometry_border'].disjoint(row['geometry_border']))
+        neighbors = df[~df.geometry_border.disjoint(row.geometry_border)].plz.tolist()
+        #  # remove own name from the list
+        #  neighbors = [ name for name in neighbors if row.plz != name ]
+        #  # add names of neighbors as NEIGHBORS value
+        #  df.at[index, "NEIGHBORS"] = ", ".join(neighbors)
+#
+    print(type(df))
 
 if __name__ == "__main__":
 
@@ -21,5 +42,6 @@ if __name__ == "__main__":
     shp_file  = "/Users/lukasbahr/POM/political_districting/data/plz-5stellig.shp"
     csv_zuordnung  = "/Users/lukasbahr/POM/political_districting/data/zuordnung_plz_ort.csv"
 
-    df = extractdata.getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung)
-    createGraph(df)
+    df_Border, df_Center = extractdata.getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung)
+    #  createGraph(df)
+    findSharedBorders(df_Border)
