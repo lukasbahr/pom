2b811fe4d1adc82ae3399eb1a351498908902985
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2020-07-11 13:18:59 +0200

    updated files

---
+++ political_districting/helperfunctions.py
@@ -0,0 +1,87 @@
+import extractdata
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import networkx as nx
+from gurobipy import *
+
+
+def plotMap(df_Border, df_Center):
+    df_Border = findSharedBorders(df_Border)
+    G = createGraph(df_Border)
+
+
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    # fix style
+    for x, y in G.edges:
+        for index, row in df_Center.iterrows():
+            if row['plz'] == x:
+                point_1 = [row.geometry.centroid.x, row.geometry.centroid.y]
+            elif row['plz'] == y:
+                point_2 = [row.geometry.centroid.x, row.geometry.centroid.y]
+            else:
+                continue
+
+        plt.plot([point_1[0], point_2[0]], [point_1[1], point_2[1]],
+            color='k', linestyle = '-', linewidth =1.0)
+
+    df_Border.plot(label='Border', ax=ax, color='white', edgecolor='black')
+    df_Center.plot(label='Center', ax=ax, marker='o', color='red', markersize=5)
+
+    plt.axis('off')
+    plt.show()
+
+
+def createGraph(df_Border):
+    G = nx.Graph()
+    for index, row in df_Border.iterrows():
+        G.add_node(row['plz'], population=row['einwohner'])
+        G.add_edges_from((row['plz'], x) for x in row['neighbours'])
+
+
+def createGraph(df_Border):
+    df_Border = findSharedBorders(df_Border)
+    G = nx.Graph()
+    for index, row in df_Border.iterrows():
+        G.add_node(row['plz'], population=row['einwohner'])
+        for neighbour in row['neighbours']:
+            G.add_edge(row['plz'], neighbour, capacity = 1)
+
+    return G
+
+
+def findSharedBorders(df_Border):
+    df_Border["neighbours"] = None
+
+    for index, row in df_Border.iterrows():
+        neighbors = df_Border[~df_Border.geometry.disjoint(row.geometry)].plz.tolist()
+        neighbors = [ plz for plz in neighbors if row.plz != plz ]
+        df_Border.at[index, "neighbours"] = neighbors
+
+    return df_Border
+
+
+def plotGraph(G, df_Center):
+    #Extract Centroid Coordinates as Dict to support nx.draw
+    coords = df_Center.set_index("plz").T.to_dict('list')
+    coords = {plz:[coords[plz][2][0].x, coords[plz][2][0].y] for plz in coords}
+    #Draw Graph
+    nx.draw(G, pos = coords, with_labels = True, node_color = 'r')
+    plt.show()
+
+
+#  if __name__ == "__main__":
+    #
+    #  shp_file_centeroid = "data/plz-5stellig-centroid.shp"
+    #  shp_file  = "data/plz-5stellig.shp"
+    #  csv_zuordnung  = "data/zuordnung_plz_ort.csv"
+    #
+    #  df_Border, df_Center = extractdata.getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung)
+    #  plotMap(df_Border, df_Center)
+
+    # Matthias
+    #  G = createGraph(df_Border)
+    #solve(G, 2, 290000)
+    #  plotGraph(G, df_Center)
--- political_districting/politicaldistricting.py
+++ political_districting/politicaldistricting.py
@@ -1,4 +1,5 @@
 import extractdata
+import helperfunctions
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
@@ -27,40 +28,6 @@ def solve(G, k, req_p):
 
     return model
 
-def plotMap(df_Border, df_Center):
-    df_Border = findSharedBorders(df_Border)
-    G = createGraph(df_Border)
-
-
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-
-    # fix style
-    for x, y in G.edges:
-        for index, row in df_Center.iterrows():
-            if row['plz'] == x:
-                point_1 = [row.geometry.centroid.x, row.geometry.centroid.y]
-            elif row['plz'] == y:
-                point_2 = [row.geometry.centroid.x, row.geometry.centroid.y]
-            else:
-                continue
-
-        plt.plot([point_1[0], point_2[0]], [point_1[1], point_2[1]],
-            color='k', linestyle = '-', linewidth =1.0)
-
-    df_Border.plot(label='Border', ax=ax, color='white', edgecolor='black')
-    df_Center.plot(label='Center', ax=ax, marker='o', color='red', markersize=5)
-
-    plt.axis('off')
-    plt.show()
-
-
-def createGraph(df_Border):
-    G = nx.Graph()
-    for index, row in df_Border.iterrows():
-        G.add_node(row['plz'], population=row['einwohner'])
-        G.add_edges_from((row['plz'], x) for x in row['neighbours'])
-
 def cb_sep_violation(model, where):
     global sep_count
     sep_count = 0
@@ -73,36 +40,6 @@ def cb_sep_violation(model, where):
             G_sub = G.subgraph(assignedNodes)
             #check connectivity of all nodes on this subgraph
 
-def createGraph(df_Border):
-    df_Border = findSharedBorders(df_Border)
-    G = nx.Graph()
-    for index, row in df_Border.iterrows():
-        G.add_node(row['plz'], population=row['einwohner'])
-        for neighbour in row['neighbours']:
-            G.add_edge(row['plz'], neighbour, capacity = 1)
-
-    return G
-
-
-def findSharedBorders(df_Border):
-    df_Border["neighbours"] = None
-
-    for index, row in df_Border.iterrows():
-        neighbors = df_Border[~df_Border.geometry.disjoint(row.geometry)].plz.tolist()
-        neighbors = [ plz for plz in neighbors if row.plz != plz ]
-        df_Border.at[index, "neighbours"] = neighbors
-
-    return df_Border
-
-
-def plotGraph(G, df_Center):
-    #Extract Centroid Coordinates as Dict to support nx.draw
-    coords = df_Center.set_index("plz").T.to_dict('list')
-    coords = {plz:[coords[plz][2][0].x, coords[plz][2][0].y] for plz in coords}
-    #Draw Graph
-    nx.draw(G, pos = coords, with_labels = True, node_color = 'r')
-    plt.show()
-
 
 if __name__ == "__main__":
 
@@ -111,9 +48,7 @@ if __name__ == "__main__":
     csv_zuordnung  = "data/zuordnung_plz_ort.csv"
 
     df_Border, df_Center = extractdata.getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung)
-    plotMap(df_Border, df_Center)
+    G = helperfunctions.createGraph(df_Border)
 
-    # Matthias
-    #  G = createGraph(df_Border)
-    #solve(G, 2, 290000)
-    #  plotGraph(G, df_Center)
+    print(list(G.nodes))
+
