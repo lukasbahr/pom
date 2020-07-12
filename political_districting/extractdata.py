24703ab0d03c61a36a25ef62b0af916ac956abb5
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2020-07-12 13:40:53 +0200

    updated version for pep8

--- political_districting/extractdata.py
+++ political_districting/extractdata.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import geopandas as gpd
 
-def getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung):
+def getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung,
+        state = "Saarland"):
+    """
+    Return border and center geopanda dataframe containing all plz of
+    a specific state. Default state being Saarland.
 
-    df_PLZ_BL = getDF_CSVZuordnung(csv_zuordnung)
+    """
+    # Create drop list of plz not in a state
+    df_PLZ_BL = getDF_CSVZuordnung(csv_zuordnung, state)
     drop = list(df_PLZ_BL['plz'])
 
-    df_Border = getDF_SHPFile(shp_file, drop)
-    #df_Border.to_csv("/Users/matthias/dev/pom/political_districting/data/shp_file.csv", sep = '\t', index = False)
-    df_Center = getDF_SHPFileCenteroid(shp_file_centeroid, drop)
-    #df_Center.to_csv("/Users/matthias/dev/pom/political_districting/data/shp_file_centroid.csv", sep = '\t', index = False)
+    # Drop plz in geopanda dataframes
+    df_Border = dropPLZ(shp_file, drop)
+    df_Center = dropPLZ(shp_file_centeroid, drop)
 
+    # Check if rows of dataframes are matching
     if len(df_Border)  == len(df_Center) == len(df_PLZ_BL):
-        df_Border = df_Border.merge(df_PLZ_BL[['bundesland', 'plz']], left_on = 'plz', right_on = 'plz')
-        df_Center = df_Center.merge(df_PLZ_BL[['bundesland', 'plz']], left_on = 'plz', right_on = 'plz')
+        # Merge state column to geopanda dataframe
+        df_Border = df_Border.merge(df_PLZ_BL[['bundesland', 'plz']], left_on =
+                'plz', right_on = 'plz')
+        df_Center = df_Center.merge(df_PLZ_BL[['bundesland', 'plz']], left_on =
+                'plz', right_on = 'plz')
 
         return df_Border, df_Center
 
@@ -21,27 +30,31 @@ def getPolititcalDistrictData(shp_file_centeroid, shp_file, csv_zuordnung):
         print("Rows of dataframes are not matching.")
 
 
-def getDF_SHPFileCenteroid(path, drop):
+def dropPLZ(path, drop_plz):
+    """
+    Return a geopanda dataframe for only specified plz.
+
+    """
     df = gpd.read_file(path)
-    df.drop(df[~df['plz'].str.contains('|'.join(str(x) for x in drop))].index, axis=0, inplace=True)
+    # Drop all not necessary plz
+    df.drop(df[~df['plz'].str.contains('|'.join(str(x) for x in
+        drop_plz))].index, axis=0, inplace=True)
     df.drop(['note'], axis=1, inplace = True)
     df['plz'] = df['plz'].astype(int)
 
     return df
 
-def getDF_SHPFile(path, drop):
-    df = gpd.read_file(path)
-    df.drop(df[~df['plz'].str.contains('|'.join(str(x) for x in drop))].index, axis=0, inplace=True)
-    df.drop(['note'], axis=1, inplace = True)
-    df['plz'] = df['plz'].astype(int)
 
-    return df
+def getDF_CSVZuordnung(path, state):
+    """
+    Return a panda dataframe with plz of a specified state.
+    For state = "Germany" return all plz of Germany.
 
-def getDF_CSVZuordnung(path):
+    """
     df = pd.read_csv(path)
     df.drop_duplicates(subset ="plz", keep = 'first', inplace = True)
 
-    # Comment to get all zip codes for germany
-    df.drop(df[df['bundesland'] != "Saarland"].index, axis=0, inplace=True)
+    if state != "Germany":
+        df.drop(df[df['bundesland'] != state].index, axis=0, inplace=True)
 
     return df
